34ca8842519e0449adcca793f0fbeea64d540a17
momokahavailable <balaps790@gmail.com>
2025-06-20 08:54:24 +0700

    v1.12

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,12 +6,19 @@
   <div class="bg-white-50 border border-gray-200 rounded-lg p-6 shadow-md mb-6">
     <h1 class="text-3xl font-bold text-gray-800 mb-4">Dashboard</h1>
     <p class="text-gray-600">Dashboard-admin</p>
-    
   </div>
-   <div class="bg-grey-50 border border-white-200 rounded-lg p-6 shadow-md">
-  <h2 class="text-xl font-semibold text-black-700">Profil Advokat</h2>
-  <p class="text-gray-600">Clara Wijaya</p>
-  <p class="text-sm text-gray-500">Terakhir aktif 2 jam lalu</p>
-</div>
 
+  <div class="bg-grey-50 border border-white-200 rounded-lg p-6 shadow-md flex justify-between items-center">
+    <div>
+      <h2 class="text-xl font-semibold text-black-700">Profil Advokat</h2>
+      <p class="text-gray-600">Clara Wijaya</p>
+      <p class="text-sm text-gray-500">Terakhir aktif 2 jam lalu</p>
+    </div>
+    
+    <div>
+      <button class="bg-red-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow">
+        Segera Aktifkan Akun
+      </button>
+    </div>
+  </div>
 @endsection
